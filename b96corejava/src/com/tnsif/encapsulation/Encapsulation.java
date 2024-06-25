ba895cfd6806818f10d25604339df9c9cf2f7e99
Anvitha K P <Anvitha K P@LAPTOP-S9LG9LBV>
2024-06-25 10:34:31 +0530

    first commit

---
+++ b96corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+//Program to Demonstrate Encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum=10; //data member
+	String name;
+	int age;
+	
+	void show()  //member function
+	{
+		System.out.println("serialnumber"+serialNum+"\t name"+name+"\t age:"+age);
+	}
+
+}
---
+++ b96corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Encapsulation ob= new Encapsulation();
+		ob.name="Anvitha";
+		ob.age=20;
+		ob.serialNum=10;
+		ob.show();
+
+	}
+
+}
